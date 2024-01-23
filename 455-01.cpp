d3e8beb20996bbe87b5f04f695742ff4706efcd0
shahid-pro <70672146+shahid-pro@users.noreply.github.com>
2024-01-23 22:40:17 +0530

    Add files via upload
    basic Input output leading to a start

---
+++ 455-01.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int i;
+   
+    cin >> i ;
+   
+
+    if (i >= 65 && i <= 90) {
+        
+        cout << "1";
+    }
+	 else if (i >= 97 && i <= 122) {
+        
+        cout << "1";
+     }
+	else {
+    
+    cout << "-1";
+        }
+    
+    
+    return 0;
+}
